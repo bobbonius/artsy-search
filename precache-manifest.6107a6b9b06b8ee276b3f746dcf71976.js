79d0be231b64c23c8761305526b1d32c2f1e3b36
Bob Muller <bobmuller.f@gmail.com>
2019-12-16 21:40:00 +0100

    Updates

---
+++ precache-manifest.6107a6b9b06b8ee276b3f746dcf71976.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc1171f83579e53fb390e7b763cfce6",
+    "url": "/artsy-search/index.html"
+  },
+  {
+    "revision": "756e40b212058adb1854",
+    "url": "/artsy-search/static/css/main.8be3e07f.chunk.css"
+  },
+  {
+    "revision": "93c3943c452b6f3e46f6",
+    "url": "/artsy-search/static/js/2.25c9f150.chunk.js"
+  },
+  {
+    "revision": "756e40b212058adb1854",
+    "url": "/artsy-search/static/js/main.b74836f5.chunk.js"
+  },
+  {
+    "revision": "f8582ad299fb04f7035f",
+    "url": "/artsy-search/static/js/runtime~main.aa5e37b8.js"
+  }
+]);
