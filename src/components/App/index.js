a84a4eac7ca694e4dc080872d3d47df66a9ed850
Bob Muller <bobmuller.f@gmail.com>
2019-12-16 20:44:05 +0100

    initial skeleton setup, using @apollo/react-hooks now

---
+++ src/components/App/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  display: grid;
+  grid-gap: 1.5rem;
+  position: relative;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  max-width: 35rem;
+  margin: 2rem auto;
+`;
+
+const App = () => {
+  return (
+    <>
+      <Container>
+      </Container>
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
+import App from "../src/components/App"
 import "./index.css";
 
-const client = new ApolloClient({
-  uri: "https://metaphysics-production.artsy.net"
-});
-
-const ApolloApp = () => (
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
-);
-
-ReactDOM.render(<ApolloApp />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
